7babf0c76746cfe5a33db7a9923b5c2f8f341e64
Dangercoder <emil.jarvinen@entercash.com>
2019-02-19 14:41:12 +0100

    Created application property file.

--- egghead-swish/swish-create-payment/src/main/java/egghead/swish/swishcreatepayment/SpringConfiguration.java
+++ egghead-swish/swish-create-payment/src/main/java/egghead/swish/swishcreatepayment/SpringConfiguration.java
@@ -1,10 +1,35 @@
 package egghead.swish.swishcreatepayment;
 
+import com.fasterxml.jackson.databind.deser.std.NumberDeserializers;
+import com.fasterxml.jackson.databind.deser.std.StringDeserializer;
+import com.google.common.collect.ImmutableMap;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import reactor.kafka.receiver.ReceiverOptions;
+
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author mikael
  */
 @Configuration
 public class SpringConfiguration {
+    @Value("")
+    private String bootstrapServers;
+
+    private ReceiverOptions create() {
+        Map<String, Object> props = new ImmutableMap.Builder<String, Object>()
+            .put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers)
+            .put(ConsumerConfig.CLIENT_ID_CONFIG, "sample-consumer")
+            .put(ConsumerConfig.GROUP_ID_CONFIG, "sample-group")
+            .put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, NumberDeserializers.IntegerDeserializer.class)
+            .put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class)
+            .put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
+            .build();
+        return ReceiverOptions.create(props);
+    }
 }
