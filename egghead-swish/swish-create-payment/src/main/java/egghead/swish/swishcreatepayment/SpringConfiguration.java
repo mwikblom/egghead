d97bfeaa7fa5f4b3c0b497ff9c204a6d5137a785
Markus Flambard <markus@flambard.se>
2019-02-19 16:49:18 +0100

    Next level stuff

--- egghead-swish/swish-create-payment/src/main/java/egghead/swish/swishcreatepayment/SpringConfiguration.java
+++ egghead-swish/swish-create-payment/src/main/java/egghead/swish/swishcreatepayment/SpringConfiguration.java
@@ -2,16 +2,26 @@ package egghead.swish.swishcreatepayment;
 
 import com.google.common.collect.ImmutableMap;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.IntegerDeserializer;
+import org.apache.kafka.common.serialization.IntegerSerializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import reactor.core.Disposable;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.kafka.receiver.KafkaReceiver;
 import reactor.kafka.receiver.ReceiverOffset;
 import reactor.kafka.receiver.ReceiverOptions;
 import reactor.kafka.receiver.ReceiverRecord;
+import reactor.kafka.sender.KafkaSender;
+import reactor.kafka.sender.SenderOptions;
+import reactor.kafka.sender.SenderRecord;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
@@ -30,7 +40,12 @@ public class SpringConfiguration {
     private String bootstrapServers;
     @Value("${kafka.swish.request.topic}")
     private String swishRequestTopic;
+
+    @Value("${kafka.swish.response.topic}")
+    private String swishResponseTopic;
+
     private Disposable kafkaConsumer;
+    private KafkaSender<Integer, String> kafkaSender;
 
     @PostConstruct
     public void setupKafkaConsumer() {
@@ -49,6 +64,17 @@ public class SpringConfiguration {
         return ReceiverOptions.create(props);
     }
 
+    private SenderOptions<Integer, String> createSenderOptions() {
+        Map<String, Object> props = new ImmutableMap.Builder<String, Object>()
+            .put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers)
+            .put(ProducerConfig.CLIENT_ID_CONFIG, "sample-producer")
+            .put(ProducerConfig.ACKS_CONFIG, "all")
+            .put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, IntegerSerializer.class)
+            .put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class)
+            .build();
+        return SenderOptions.create(props);
+    }
+
     private Disposable createKafkaConsumer(String topic) {
 
         ReceiverOptions<Integer, String> options = createReceiverOptions()
@@ -65,12 +91,25 @@ public class SpringConfiguration {
                 new SimpleDateFormat("HH:mm:ss:SSS z dd MMM yyyy").format(new Date(record.timestamp())),
                 record.key(),
                 record.value());
+            Mono<SenderRecord<Integer, String, Integer>> message = Mono.just(SenderRecord.create(swishResponseTopic, null, null, null, "Message_" + record.value(), null));
+            kafkaSender.send(message);
             offset.acknowledge();
         });
+    }
+
+    @Bean
+    public KafkaSender<Integer, String> getKafkaSender() {
+        kafkaSender = KafkaSender.create(createSenderOptions());
+        return kafkaSender;
     }
 
     @PreDestroy
     public void disposeKafkaConsumer() {
         kafkaConsumer.dispose();
+    }
+
+    @PreDestroy
+    public void closeKafkaSender() {
+        kafkaSender.close();
     }
 }
